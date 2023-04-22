e23dee96fd57146d1166f27e5b13f45dd32fdab4
Jakub Adler <adler.jakub@gmail.com>
2023-04-22 21:09:20 +0200

    feat: Create comment for project
    Project contains form for comment with hidden input for project id
    view

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -58,6 +58,7 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
   config.include FactoryBot::Syntax::Methods
+  config.include RSpecHtmlMatchers
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
--- spec/views/projects/show.html.erb_spec.rb
+++ spec/views/projects/show.html.erb_spec.rb
@@ -1,18 +1,39 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-RSpec.describe "projects/show", type: :view do
-  before(:each) do
-    assign(:project, Project.create!(
-      title: "Title",
-      address: "Address",
-      description: "MyText"
-    ))
+RSpec.describe 'projects/show' do
+  let(:project) { create(:project, title: 'Title', address: 'Address', description: 'MyText') }
+
+  before do
+    assign(:project, project)
+    assign(:comment, build(:comment))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
     expect(rendered).to match(/Title/)
     expect(rendered).to match(/Address/)
     expect(rendered).to match(/MyText/)
+  end
+
+  it 'renders project with comment form' do
+    render
+
+    expect(rendered).to have_tag('form', with: { action: '/comments', method: :post }) do
+      with_tag 'input', with: { name: 'comment[text]' }
+      with_tag 'input', with: { name: 'comment[project_id]', value: project.id, type: :hidden }
+    end
+  end
+
+  context 'when Project has a message' do
+    let(:project_with_comment) { create(:project) }
+    let!(:comment) { create(:comment, text: 'Some message', project: project_with_comment) }
+
+    before { assign(:project, project_with_comment) }
+
+    it 'will contain the message text' do
+      render
+
+      expect(rendered).to match(/Some message/)
+    end
   end
 end
